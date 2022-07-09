74880d1100ed0b809e832aa2754ec7158ea9a02d
unknown <alimardon007@gmail.com>
2022-07-09 22:32:55 +0300

    1 commit

---
+++ app.js
@@ -0,0 +1,30 @@
+//jshing esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+let items = [];
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = today.getFullYear();
+
+  today = dd + "." + mm + "." + yyyy;
+  res.render("list", { kindOfDay: today, newItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started on port 3000.");
+});
