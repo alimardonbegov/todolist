642c24a446da457e35962293e1552351cee33005
unknown <alimardon007@gmail.com>
2022-07-09 22:57:44 +0300

    2 commit

--- app.js
+++ app.js
@@ -2,29 +2,44 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-let items = [];
+const items = [];
+const workItems = [];
 
+//--------------- When server get MAIN page ('/')-----------//
 app.get("/", function (req, res) {
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, "0");
-  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  var yyyy = today.getFullYear();
-
-  today = dd + "." + mm + "." + yyyy;
-  res.render("list", { kindOfDay: today, newItems: items });
+  let day = date.getDate();
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    // ONLY WORK, BECAUSE listTitle may equal "Work List" with space
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
-app.listen(process.env.PORT || 3000, function () {
+//--------------- When server get WORK page ('/work')-----------//
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+//--------------- When server get ABOUT page ('/about')-----------//
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function () {
   console.log("Server started on port 3000.");
 });
