6ae9448e73d620e0871b7cd112ef8b068cfc67d3
unknown <alimardon007@gmail.com>
2022-07-10 12:48:41 +0300

    first test commit

--- app.js
+++ app.js
@@ -1,43 +1,14 @@
-//jshing esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-const items = [];
-const workItems = [];
-
-//--------------- When server get MAIN page ('/')-----------//
 app.get("/", function (req, res) {
-  let day = date.getDate();
-  res.render("list", { listTitle: day, newListItems: items });
-});
-
-app.post("/", function (req, res) {
-  let item = req.body.newItem;
-  if (req.body.list === "Work") {
-    // ONLY WORK, BECAUSE listTitle may equal "Work List" with space
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-});
-
-//--------------- When server get WORK page ('/work')-----------//
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
-});
-
-//--------------- When server get ABOUT page ('/about')-----------//
-app.get("/about", function (req, res) {
-  res.render("about");
+  res.send("Hello");
 });
 
 app.listen(process.env.PORT || 3000, function () {
